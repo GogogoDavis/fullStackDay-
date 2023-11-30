b2689e363eb003d5a097fe4696a531ba988a8401
chase <chasegoodrow@gmail.com>
2023-11-30 12:10:48 -0700

    update thingy

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 export const Home = () => {
   const [todo_data, setTodo_data] = useState()
   const [newItem, setNewItem] = useState()
+  const [updateText, setUpdateText] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:8080/')
@@ -21,9 +22,6 @@ export const Home = () => {
   const handleInputChange = (e) => {
     setNewItem(e.target.value)
   }
-  const handleUpdateItem = (e) => {
-    
-  }
 
   const handleDelete = (id) => { 
     const queryParams = `?id=${id}`
@@ -33,21 +31,17 @@ export const Home = () => {
     })
   }
 
-const handleUpdate = () => { 
-  const queryParams = `?updateItem=${updateItem}`
-  fetch(`http://localhost:8080/delete/${queryParams}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-})
-  return ( 
-    <form>
-      <label>
-          <input type="text" value={updateItem} onChange={handleUpdateItem}></input>
-        </label>
-      <button type='submit'>update</button>
-    </form>
-  )
-}
+  const sendUpdate = (id) => {
+    
+  }
+
+  const handleUpdate = (id) => {
+    setTodo_data((prevData) =>
+      prevData.map((item) =>
+        item.id === id ? { ...item, isEditing: !item.isEditing } : item
+      )
+    );
+  };
 
   return (
     <>
@@ -55,7 +49,23 @@ const handleUpdate = () => {
         {todo_data ?
         <ol>
           {todo_data.map(item => {
-            return (<><button onClick={() => handleDelete(item.id)}>X</button><li>{item.toDo}</li><button onClick={() => handleUpdate()}></button></>)
+            return (<>
+              <button onClick={() => handleDelete(item.id)}>X</button>
+              <li>{item.toDo}</li>
+              {item.isEditing ? 
+                <form onSubmit={(e) => {handleUpdate(item.id); sendUpdate(item.id)}}>
+                  <label>
+                    <input type="text" value={updateText} onChange={(e) => setUpdateText(e.target.value)} />
+                  </label>
+                  <button type="submit">Update</button>
+                </form>
+              : 
+                <>
+                  {item.toDo}
+                  <button onClick={() => handleUpdate(item.id)}>Edit</button>
+                </>
+              }
+            </>)
           })}
         </ol>
         : <></>}
